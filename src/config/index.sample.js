4e2c0d1737f8320145fbef274f540918137e609d
Riad Benguella <benguella@gmail.com>
2020-06-06 12:32:42 +0100

    Fix sample config

--- src/config/index.sample.js
+++ src/config/index.sample.js
@@ -2,7 +2,7 @@ export const config = {
   firebase: {
     apiKey: "${{ secrets.FIREBASE_API_KEY }}",
     databaseURL: "${{ secrets.FIREBASE_DATABASE_URL }}",
-    projectId: "${{ secrets.FIREBASE_PROJECT_URL }}",
+    projectId: "${{ secrets.FIREBASE_PROJECT_ID }}",
     messagingSenderId: "${{ secrets.FIREBASE_MESSAGING_SENDER_ID }}",
     appId: "${{ secrets.FIREBASE_APP_ID }}",
     measurementId: "${{ secrets.FIREBASE_MEASUREMENT_ID }}",
