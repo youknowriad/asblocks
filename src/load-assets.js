309320f5d6873087b909906786f600097b074ddc
Riad Benguella <benguella@gmail.com>
2020-06-06 22:50:32 +0100

    limit allowed blocks to the static blocks

--- src/load-assets.js
+++ src/load-assets.js
@@ -10,6 +10,38 @@ import "@wordpress/notices";
 
 import "./editor-styles.css";
 
+import { getBlockTypes, unregisterBlockType } from "@wordpress/blocks";
 import { registerCoreBlocks } from "@wordpress/block-library";
 import "@wordpress/format-library";
+
 registerCoreBlocks();
+
+const allowedBlocks = [
+  "core/paragraph",
+  "core/heading",
+  "core/image",
+  "core/list",
+  "core/quote",
+  "core/cover",
+  "core/audio",
+  "core/video",
+  "core/code",
+  "core/preformatted",
+  "core/html",
+  "core/pullquote",
+  "core/table",
+  "core/verse",
+  "core/buttons",
+  "core/button",
+  "core/columns",
+  "core/column",
+  "core/group",
+  "core/separator",
+  "core/spacer",
+];
+
+getBlockTypes().forEach(({ name }) => {
+  if (!allowedBlocks.includes(name)) {
+    unregisterBlockType(name);
+  }
+});
