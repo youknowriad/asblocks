1cf3ae108282edc49c6e49742f6a0ffd47480229
Riad Benguella <benguella@gmail.com>
2020-06-09 11:21:08 +0100

    Fix logo click hard refresh

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,16 +1,16 @@
+import { Button } from "@wordpress/components";
 import "./style.css";
-import { ButtonLink } from "../button-link";
 
 export function Logo() {
   return (
-    <ButtonLink
-      to="/"
+    <Button
+      href="/"
       className="logo"
       aria-label="Start a new post"
       showTooltip
       label="Start a new post"
     >
       <span>A</span>B
-    </ButtonLink>
+    </Button>
   );
 }
