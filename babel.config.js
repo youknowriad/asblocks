5089c96f971bb058c8316f95fb2e26c3fd34ecaf
epiqueras <epiquerass@gmail.com>
2020-06-10 16:10:34 -0700

    Add HMR and WordPress Scripts.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
-module.exports = {
-  presets: ["@wordpress/babel-preset-default"],
-  plugins: ["@babel/plugin-transform-runtime"],
+module.exports = ( api ) => {
+	api.cache.using( () => process.env.NODE_ENV );
+	return {
+		presets: [ '@wordpress/babel-preset-default' ],
+		plugins: [
+			'@babel/plugin-transform-runtime',
+			api.env( 'development' ) && 'react-refresh/babel',
+		].filter( Boolean ),
+	};
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,43 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require( 'path' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
+const ReactRefreshWebpackPlugin = require( '@pmmmwh/react-refresh-webpack-plugin' );
 
+const isDevelopment = process.env.NODE_ENV === 'development';
 module.exports = {
-  mode: process.env.NODE_ENV === "development" ? "development" : "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-  },
-  devServer: {
-    contentBase: "./dist",
-    index: "index.html",
-    historyApiFallback: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "AsBlocks, encrypted collaborative writing environment",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+	mode: isDevelopment ? 'development' : 'production',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve( __dirname, 'dist' ),
+		publicPath: '/',
+	},
+	devServer: {
+		contentBase: './dist',
+		index: 'index.html',
+		historyApiFallback: true,
+		hot: true,
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin( {
+			title: 'AsBlocks, encrypted collaborative writing environment',
+		} ),
+		isDevelopment && new ReactRefreshWebpackPlugin(),
+	].filter( Boolean ),
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
 };
