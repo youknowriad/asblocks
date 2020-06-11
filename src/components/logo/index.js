170dc0745f604e3dca08d0ee525e8ce3a7169977
Riad Benguella <benguella@gmail.com>
2020-06-11 01:01:57 +0100

    Add beta mention

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -12,6 +12,7 @@ export function Logo() {
           aria-haspopup="true"
           aria-expanded={isOpen}
           label="About AsBlocks"
+          showTooltip
           icon={
             <svg
               width="18"
@@ -22,7 +23,9 @@ export function Logo() {
               <path d="M7 0h4v7h7v4h-4.1716l2.9498 2.9497-2.8285 2.8285L11 13.8284V18H7v-7H0V7h4.17155L1.2218 4.05025l2.82843-2.82843L7 4.17159V0z" />
             </svg>
           }
-        />
+        >
+          <span className="logo-beta">beta</span>
+        </Button>
       )}
       renderContent={() => (
         <div className="logo-dropdown">
