3024a88a37e3724134fa55298b7436ae9df52c46
Riad Benguella <benguella@gmail.com>
2020-06-12 01:16:29 +0100

    Sync block deletion

--- src/components/editor/sync.js
+++ src/components/editor/sync.js
@@ -327,7 +327,7 @@ export function useSyncEdits( post, onChange, encryptionKey, ownerKey ) {
 				return false;
 			}
 
-			deletedBlocks.current = getDeletedBlocks(
+			const newDeletedBlocks = getDeletedBlocks(
 				blocks.current,
 				post.blocks
 			);
@@ -352,16 +352,22 @@ export function useSyncEdits( post, onChange, encryptionKey, ownerKey ) {
 					newPositionVersions,
 					positionVersions.current
 				) &&
+				isShallowEqualObjects(
+					newDeletedBlocks,
+					deletedBlocks.current
+				) &&
 				post.title === lastPersisted.current.title
 			) {
 				blockVersions.current = newBlockVersions;
 				positionVersions.current = newPositionVersions;
+				deletedBlocks.current = newDeletedBlocks;
 				blocks.current = post.blocks;
 				lastPersisted.current = post;
 				return false;
 			}
 			blockVersions.current = newBlockVersions;
 			positionVersions.current = newPositionVersions;
+			deletedBlocks.current = newDeletedBlocks;
 			blocks.current = post.blocks;
 			lastPersisted.current = post;
 
