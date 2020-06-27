b63359b372d2b2211b576c71672047510abda521
Riad Benguella <benguella@gmail.com>
2020-06-27 10:13:18 +0100

    Move to parcel2 (#32)

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,6 @@ module.exports = ( api ) => {
 	api.cache.using( () => process.env.NODE_ENV );
 	return {
 		presets: [ '@wordpress/babel-preset-default' ],
-		plugins: [
-			'@babel/plugin-transform-runtime',
-			api.env( 'development' ) && 'react-refresh/babel',
-		].filter( Boolean ),
+		plugins: [ '@babel/plugin-transform-runtime' ].filter( Boolean ),
 	};
 };
