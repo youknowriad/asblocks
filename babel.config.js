8a8fdce5c29ff43b542b6aac2d4c6431e0d30592
Riad Benguella <benguella@gmail.com>
2020-12-13 09:48:53 +0100

    Update dependencies (#51)

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = ( api ) => {
-	api.cache.using( () => process.env.NODE_ENV );
-	return {
-		presets: [ '@wordpress/babel-preset-default' ],
-		plugins: [ '@babel/plugin-transform-runtime' ].filter( Boolean ),
-	};
-};
