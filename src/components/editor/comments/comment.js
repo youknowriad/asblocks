eba3c2e5513653b8b77d01c5ac50eeb17b0c1167
Riad Benguella <benguella@gmail.com>
2020-12-19 08:46:18 +0100

    Update the WordPress packages

--- src/components/editor/comments/comment.js
+++ src/components/editor/comments/comment.js
@@ -1,113 +1,106 @@
 import classnames from 'classnames';
-import { Button, withFocusOutside } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 import { Icon, people } from '@wordpress/icons';
 import { useDispatch } from '@wordpress/data';
-import { forwardRef, useImperativeHandle } from '@wordpress/element';
 import { ENTER, SPACE } from '@wordpress/keycodes';
+import { __experimentalUseFocusOutside } from '@wordpress/compose';
 import { useAuthorId } from '../../../local-storage';
 import { CommentForm } from '../comment-form';
 import { CommentReplies } from '../comment-replies';
 
-export const Comment = withFocusOutside(
-	forwardRef( ( { comment, position, isSelected }, ref ) => {
-		const [ authorId ] = useAuthorId();
-		const { updateComment, removeComment, selectComment } = useDispatch(
-			'asblocks'
-		);
-		const { clearSelectedBlock } = useDispatch( 'core/block-editor' );
-		useImperativeHandle( ref, () => ( {
-			handleFocusOutside: () => {
-				selectComment( null );
-			},
-		} ) );
-		const isBeingEdited =
-			comment.status === 'draft' && authorId === comment.authorId;
-		const update = ( content ) => {
-			updateComment( comment._id, {
-				...comment,
-				content,
-				status: undefined,
-			} );
-		};
-		const cancel = () => {
-			removeComment( comment._id );
-		};
-		const resolve = () => {
-			updateComment( comment._id, {
-				...comment,
-				status: 'resolved',
-			} );
-		};
-		const onReply = ( reply ) => {
-			updateComment( comment._id, {
-				...comment,
-				replies: [ ...( comment.replies ?? [] ), reply ],
-			} );
-		};
-		const select = () => {
-			clearSelectedBlock();
-			selectComment( comment._id );
-		};
+export function Comment( { comment, position, isSelected } ) {
+	const [ authorId ] = useAuthorId();
+	const { updateComment, removeComment, selectComment } = useDispatch(
+		'asblocks'
+	);
+	const { clearSelectedBlock } = useDispatch( 'core/block-editor' );
+	const focusOutsideProps = __experimentalUseFocusOutside( () =>
+		selectComment( null )
+	);
+	const isBeingEdited =
+		comment.status === 'draft' && authorId === comment.authorId;
+	const update = ( content ) => {
+		updateComment( comment._id, {
+			...comment,
+			content,
+			status: undefined,
+		} );
+	};
+	const cancel = () => {
+		removeComment( comment._id );
+	};
+	const resolve = () => {
+		updateComment( comment._id, {
+			...comment,
+			status: 'resolved',
+		} );
+	};
+	const onReply = ( reply ) => {
+		updateComment( comment._id, {
+			...comment,
+			replies: [ ...( comment.replies ?? [] ), reply ],
+		} );
+	};
+	const select = () => {
+		clearSelectedBlock();
+		selectComment( comment._id );
+	};
 
-		const extraProps = isSelected
-			? {}
-			: {
-					role: 'button',
-					tabIndex: 0,
-					onClick: select,
-					onKeyDown: ( event ) => {
-						if (
-							ENTER === event.keyCode ||
-							SPACE === event.keyCode
-						) {
-							select();
-						}
-					},
-			  };
+	const extraProps = isSelected
+		? {}
+		: {
+				role: 'button',
+				tabIndex: 0,
+				onClick: select,
+				onKeyDown: ( event ) => {
+					if ( ENTER === event.keyCode || SPACE === event.keyCode ) {
+						select();
+					}
+				},
+		  };
 
-		return (
-			<div
-				ref={ ref }
-				className={ classnames( 'editor-comments__item', {
-					'is-selected': isSelected,
-					'is-editing': isBeingEdited,
-				} ) }
-				style={ { top: position } }
-				{ ...extraProps }
-			>
-				{ ! isBeingEdited && comment.authorName && (
-					<div className="editor-comments__item-author">
-						<Icon icon={ people } />
-						<span className="editor-comments__item-author-name">
-							{ comment.authorName }
-						</span>
-						<Button isSecondary isSmall onClick={ resolve }>
-							Resolve
-						</Button>
+	return (
+		<div
+			className={ classnames( 'editor-comments__item', {
+				'is-selected': isSelected,
+				'is-editing': isBeingEdited,
+			} ) }
+			style={ { top: position } }
+			{ ...extraProps }
+			{ ...focusOutsideProps }
+		>
+			{ ! isBeingEdited && comment.authorName && (
+				<div className="editor-comments__item-author">
+					<Icon icon={ people } />
+					<span className="editor-comments__item-author-name">
+						{ comment.authorName }
+					</span>
+					<Button isSecondary isSmall onClick={ resolve }>
+						Resolve
+					</Button>
+				</div>
+			) }
+			{ isBeingEdited && (
+				<CommentForm
+					focusOnMount
+					initialValue={ comment.content }
+					onCancel={ cancel }
+					onSubmit={ update }
+				/>
+			) }
+			{ ! isBeingEdited && (
+				<>
+					<div className="editor-comments__item-content">
+						{ comment.content }
 					</div>
-				) }
-				{ isBeingEdited && (
-					<CommentForm
-						focusOnMount
-						initialValue={ comment.content }
-						onCancel={ cancel }
-						onSubmit={ update }
-					/>
-				) }
-				{ ! isBeingEdited && (
-					<>
-						<div className="editor-comments__item-content">
-							{ comment.content }
-						</div>
-						{ isSelected && (
-							<CommentReplies
-								replies={ comment.replies }
-								onReply={ onReply }
-							/>
-						) }
-					</>
-				) }
-			</div>
-		);
-	} )
-);
+					{ isSelected && (
+						<CommentReplies
+							replies={ comment.replies }
+							onReply={ onReply }
+						/>
+					) }
+				</>
+			) }
+		</div>
+	);
+}
